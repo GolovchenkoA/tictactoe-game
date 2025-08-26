265d97248e2bf09ca4624417b2a33a419b6735b1
Artem <golovchenkoart@gmail.com>
2025-08-26 12:12:09 +0200

    init

---
+++ src/test/java/com/holovchenko/artem/game/tictactoe/TictactoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holovchenko.artem.game.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
