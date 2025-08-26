aa1fdbd9e6e2815ffea640aa9bd037236acbf186
Artem <golovchenkoart@gmail.com>
2025-08-26 15:29:17 +0200

    GameController updated. PATCH updateGame method added.

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
+
+
+## How to run the Application
+
 ```shell
 gradlew build
-#docker build -t tiktaktoe:0.0.1-SNAPSHOT .
 docker-compose up --build -d
 ```
 
+## How to stop the Application
 ```shell
-docker-compose down
+docker-compose down -v
 ```
 
 ## How to play
 
 You can import a Postman collection with all the methods below.
-See "TicTacToe REST API.postman_collection" file in the root project folder
+See `TicTacToe REST API.postman_collection` file in the root project folder
 
-Creating a game
+Creating a game.
 ```shell
 POST localhost:8080/games?player1=player1&player2=player2
 ```
@@ -23,11 +27,30 @@ Get all games
 GET localhost:8080/games
 ```
 
+Player 1 move:
+```shell
+PATCH localhost:8080/games/{gameId}?player=player1&row=1&column=1
+```
+
+Player 2 move:
+```shell
+PATCH localhost:8080/games/{gameId}?player=player1&row=2&column=2
+```
+
+
 ## Database
-MongoDB is used as a storage.
+MongoDB is used as a storage. 
+You can use [MongoDB Compass](https://www.mongodb.com/products/tools/compass) or [MongoDb Shell](https://www.mongodb.com/docs/mongodb-shell/install/) to connect to the DB.
+
+
 uri: localhost:27017
+
 login: root
+
 password: root
+
 db: tictactoe
+
 collection: games
+
 authSource: admin
--- src/main/java/com/holovchenko/artem/game/tictactoe/GameController.java
+++ src/main/java/com/holovchenko/artem/game/tictactoe/GameController.java
@@ -1,12 +1,15 @@
 package com.holovchenko.artem.game.tictactoe;
 
 import com.holovchenko.artem.game.tictactoe.db.TicTacToeGame;
+import com.holovchenko.artem.game.tictactoe.exception.IllegalTurnException;
+import com.holovchenko.artem.game.tictactoe.model.GameStatus;
 import com.holovchenko.artem.game.tictactoe.model.HumanPlayer;
 import com.holovchenko.artem.game.tictactoe.model.Symbol;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,5 +36,29 @@ public class GameController {
     @GetMapping
     public List<TicTacToeGame> getAllGames() {
         return gameService.getAllGames();
+    }
+
+    @PatchMapping("/{gameId}")
+    public ResponseEntity<?> updateGame(@PathVariable String gameId,
+                                        @RequestParam String player,
+                                        @RequestParam int row,
+                                        @RequestParam int column){
+
+        if(!isInBounds(row, column)) {
+            String msg = String.format("The board game size is 3x3. But got %dx%d", row, column);
+            throw new IllegalTurnException(msg);
+        }
+
+        TicTacToeGame game = gameService.makeMove(gameId, player, row, column);
+
+        if (game.getStatus() != GameStatus.IN_PROGRESS) {
+            return ResponseEntity.ok("Game already finished " + game.getStatus());
+        }
+
+        return ResponseEntity.ok(game);
+    }
+
+    public boolean isInBounds(int row, int column) {
+        return row >= 1 && row <= 3 && column >= 1 && column <= 3;
     }
 }
